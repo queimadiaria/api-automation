717618c5829b0c28965ed818b7517c4e0a76ec27
Arthur Bezerra de Brito <arthurbrito@QD-DSKTP027.local>
2024-08-02 18:04:46 -0300

    qnutri adjustment

--- cypress/e2e/services/qnutri/tests/getQNutri.cy.js
+++ cypress/e2e/services/qnutri/tests/getQNutri.cy.js
@@ -1,49 +1,45 @@
 import { getQnutri } from '../requests/getQNutri.request';
 
-const validateQnutriItem = (item) => {
-  expect(item).to.have.property('programid');
-  expect(item).to.have.property('programname');
-  expect(item).to.have.property('programdescription');
-  expect(item).to.have.property('objectives');
-  expect(item).to.have.property('levels');
-  expect(item).to.have.property('instructors');
-  expect(item).to.have.property('tags');
-  expect(item).to.have.property('tag');
-  expect(item).to.have.property('imagevertical');
-  expect(item).to.have.property('imagebanner');
-  expect(item).to.have.property('imagelogo');
-  expect(item).to.have.property('imagecard');
+const validateQnutriPrograms = (locale) => {
+  getQnutri(locale).then((response) => {
+    expect(response.status).to.eq(200);
+    const qnutriPrograms = response.body;
+    expect(qnutriPrograms).to.be.an('array').that.is.not.empty;
 
-  expect(item.programid).to.be.a('number');
-  expect(item.programname).to.be.a('string');
-  expect(item.programdescription).to.be.a('string');
-  expect(item.objectives).to.be.a('string');
-  expect(item.levels).to.be.a('string');
-  expect(item.instructors).to.be.a('string');
-  expect(item.tags).to.be.a('string');
-  expect(item.tag).to.be.an('array');
-  expect(item.imagevertical).to.be.a('string');
-  expect(item.imagebanner).to.be.a('string');
-  expect(item.imagelogo).to.be.a('string');
-  expect(item.imagecard).to.be.a('string');
+    qnutriPrograms.forEach(program => {
+      const expectedProperties = {
+        programId: 'number',
+        programName: 'string',
+        programDescription: 'string',
+        objectives: 'string',
+        levels: 'string',
+        instructors: 'string',
+        tags: 'string',
+        tag: 'array',
+        imageVertical: 'string',
+        imageBanner: 'string',
+        imageLogo: 'string',
+        imageCard: 'string',
+        imageCardWeb: 'string',
+      };
+
+      for (const [key, value] of Object.entries(expectedProperties)) {
+        if (typeof value === "string") {
+          expect(program).to.have.property(key).and.to.be.a(value);
+        } else {
+          expect(program).to.have.property(key, value);
+        }
+      }
+    });
+  });
 };
 
 describe('GET /qnutri with locale', () => {
-  it('should return a list of Qnutri programs in pt-br', () => {
-    getQnutri('pt-br').then((response) => {
-      expect(response.status).to.eq(200);
-
-      expect(response.body).to.be.an('array');
-
-      response.body.forEach(validateQnutriItem);
-    });
+  it('should return a list of Qnutri programs in pt-br', function() {
+    validateQnutriPrograms('pt-br');
   });
 
-  it('should return a list of Qnutri programs in es-mx', () => {
-    getQnutri('es-mx').then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.be.an('array');
-      response.body.forEach(validateQnutriItem);
-    });
+  it('should return a list of Qnutri programs in es-mx', function() {
+    validateQnutriPrograms('es-mx');
   });
 });
